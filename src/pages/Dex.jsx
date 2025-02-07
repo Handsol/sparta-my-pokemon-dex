05ec4f2404f2dc15dcadf823160a741335b7264e
Handsol <imhandsol@gmail.com>
2025-02-07 21:52:46 +0900

    refactor : addMyPokemon, removeMyPokemon 로직 수정 등 리팩토링

--- src/pages/Dex.jsx
+++ src/pages/Dex.jsx
@@ -1,47 +1,56 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import Dashboard from "../components/Dashboard";
-import styled from "styled-components";
 import PokemonList from "../components/PokemonList";
+import styled from "styled-components";
+import pokemonList from "../data/MockData";
 
 const Dex = () => {
-  const [MyPokemon, setMyPokemon] = useState(Array(6).fill(null));
+  const [myPokemon, setMyPokemon] = useState([]);
 
-  const handleMyPokemon = (pokemon) => {
-    setMyPokemon((prevPokemon) => {
-      const alreadyExists = prevPokemon.some(
-        (item) => item && item.id === pokemon.id
-      );
+  // 포켓몬리스트 -> 대쉬보드 포켓몬 추가
+  const addMyPokemon = (pokemonId) => {
+    // 추가를 누르면 포켓몬 리스트에서 값 가져오기
+    const newPokemon = pokemonList.find((pokemon) => pokemon.id === pokemonId);
 
-      if (alreadyExists) {
-        alert("이미 선택한 포켓몬입니다.");
-        return prevPokemon;
-      }
+    // 중복 포켓몬이 있는지 확인하기
+    const alreadyExists = myPokemon.some((pokemon) => pokemon.id === pokemonId);
 
-      const newPokemon = [...prevPokemon];
-      const emptyPokemon = newPokemon.findIndex((item) => item === null);
+    // 예외처리 2종
 
-      if (emptyPokemon !== -1) {
-        newPokemon[emptyPokemon] = pokemon;
-      } else {
-        alert("포켓몬은 최대 6마리까지 보유할 수 있습니다.");
-      }
+    // 만약 같은 포켓몬을 중복선택 했을 경우 알림
+    if (alreadyExists) {
+      alert("이미 데리고 있는 포켓몬입니다.");
+      return;
+    }
 
-      return newPokemon;
+    // 만약 7마리 이상의 포켓몬을 선택하려는 경우 알림
+    if (myPokemon.length === 6) {
+      alert("더이상 포켓몬을 데리고 갈 수 없습니다.");
+      return;
+    }
+
+    // 위 상황에 해당하지 않을 경우 포켓몬 추가
+    setMyPokemon((prev) => {
+      return [...prev, newPokemon];
     });
   };
 
-  const RemoveMyPokemon = (pokemon) => {
-    setMyPokemon((prevPokemon) => {
-      const newPokemon = prevPokemon.filter((item) => item !== pokemon);
-
-      return [...newPokemon, ...Array(6 - newPokemon.length).fill(null)];
+  // 대쉬보드에서 포켓몬을 삭제하는 로직
+  const removeMyPokemon = (pokemonId) => {
+    setMyPokemon((prev) => {
+      const resetPokemon = prev.filter((pokemon) => pokemon.id !== pokemonId);
+      return resetPokemon;
     });
   };
 
   return (
     <DexContainer>
-      <Dashboard MyPokemon={MyPokemon} RemoveMyPokemon={RemoveMyPokemon} />
-      <PokemonList AddMyPokemon={handleMyPokemon} />
+      <Dashboard
+        alt={MOCK_DATA}
+        myPokemon={myPokemon}
+        removeMyPokemon={removeMyPokemon}
+      />
+      <PokemonList alt={MOCK_DATA} addMyPokemon={addMyPokemon} />
     </DexContainer>
   );
 };
